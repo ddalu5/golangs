dc1a015149e112887d39d27ed6308b8e76601bba
Salah OSFOR <icarus@daedalu5.org>
2017-05-14 19:26:50 +0100

    Values

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("con" + "cat")
+	fmt.Println("result: ", 13*7)
+	fmt.Println(true && true)
+	fmt.Println(true && !false)
+	fmt.Println(true || false)
+}

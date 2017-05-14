2c113e993aaa64386ec1f298c33b62e9f8985d41
Salah OSFOR <icarus@daedalu5.org>
2017-05-14 18:37:30 +0100

    Added Hello example

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("\033[0;32m")
+	fmt.Println("Hello friend")
+	fmt.Println("I'm Poppy!")
+	fmt.Print("\033[0;31m")
+	fmt.Println("Just kidding, I just want you to read it with Poppy's voice.")
+	fmt.Print("\033[0;35m")
+	fmt.Println("I'll tell you the truth, my real name is Lain!")
+	fmt.Println("Before we start your learning process you need to \033[1mClose The World, Open the N3XT!\033[0m")
+	fmt.Println()
+	fmt.Println("\033[0;35m\033[1mRule 1#\033[0m No matter where you go, everybody's connected")
+	fmt.Println("\033[0;35m\033[1mRule 2#\033[0m The body exists only to verify one's own existence")
+	fmt.Println("\033[0;35m\033[1mRule 3#\033[0m If you’re not remembered, then you never existed")
+	fmt.Println("\033[0;35m\033[1mRule 4#\033[0m If you can hear it, then it’s speaking to you. And if you can see it, then it’s yours to have")
+	fmt.Print("\033[0m")
+	fmt.Println()
+}

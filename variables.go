5c10798fa94df0271450e27d1711d4fe311cf4b0
Salah OSFOR <icarus@daedalu5.org>
2017-05-15 08:57:05 +0100

    Variables

---
+++ variables.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a string = "This is THE string!"
+	var b, c int = 13, 37
+	var x string
+	var e int
+	fmt.Println(a)
+	fmt.Println(b, c)
+	fmt.Println(x)
+	fmt.Println(e)
+
+	shortVar := "Short V@r"
+	fmt.Println(shortVar)
+}
